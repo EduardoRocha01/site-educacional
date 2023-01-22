e6c0de82687419b215d39aff87cb296d254c79ef
EduardoRocha01 <120874588+EduardoRocha01@users.noreply.github.com>
2023-01-21 23:18:19 -0400

    att

--- script.js
+++ script.js
@@ -1,31 +1,13 @@
+// Armazena a referência ao ícone de menu e os itens do menu
+var menuIcon = document.getElementById("menu-icon");
+var menuItems = document.querySelector(".menu-items");
 
-    // Import the PayPal SDK
-    paypal.Buttons({
-        createOrder: function(data, actions) {
-            // This function sets up the details of the transaction, including the amount and line item details.
-            return actions.order.create({
-                purchase_units: [{
-                    amount: {
-                        value: '0.01'
-                    }
-                }]
-            });
-        },
-        onApprove: function(data, actions) {
-            // This function captures the funds from the transaction.
-            return actions.order.capture().then(function(details) {
-                // Show a success message to the buyer
-                alert('Transaction completed by ' + details.payer.name.given_name);
-            });
-        }
-    }).render('#paypal-button-container');
-    
-    // Add click event listener to all elements with class "compra"
-    document.querySelectorAll('.compra').forEach(function(element) {
-        element.addEventListener('click', function() {
-            // Get the paypal button container element
-            var paypalButtonContainer = this.nextElementSibling;
-            // Render the PayPal button
-            paypal.Buttons().render(paypalButtonContainer);
-        });
-    });
+// Adiciona um evento de clique ao ícone de menu
+menuIcon.addEventListener("click", function() {
+  // Alterna entre mostrar e esconder os itens do menu
+  if (menuItems.style.display === "none") {
+    menuItems.style.display = "block";
+  } else {
+    menuItems.style.display = "none";
+  }
+});
